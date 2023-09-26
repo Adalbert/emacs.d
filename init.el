496aeef373f5abe3dc07a121a99ca2f0b6035cf2
Adalbert <a.soborka@eks-gmbh.com>
2023-09-26 09:59:48 +0200

    added git-gutter that shows added/deleted/modified code blocks that haven’t been committed by git

--- init.el
+++ init.el
@@ -496,6 +496,23 @@
   :bind ("C-x g" . magit-status))
 ;;   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+
+;; -----------------------------------------------------------------------------
+;; Git Gutter
+;;   shows added/deleted/modified code blocks that haven’t been committed by git
+(use-package git-gutter
+;;  :ensure t
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+;;  :ensure t
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 ;; (setenv "GIT_ASKPASS" "git-gui--askpass")
 
 ;; -----------------------------------------------------------------------------
