bf89d0334858ee8130170d334fb1aef43eea9c15
Adalbert Soborka <a.soborka@eks-gmbh.com>
2024-07-10 15:58:20 +0200

    init commit to main branch. start with emacs from scratch

---
+++ transient/history.el
@@ -0,0 +1,21 @@
+((emoji--command-Emoji nil)
+ (emoji--command-Emoji\ >\ Body nil)
+ (magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")
+  ("-n256" "--decorate")
+  ("-n256" "--color" "--decorate" "++header"))
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil
+	     ("--dry-run")
+	     ("--force-with-lease"))
+ (magit-rebase
+  ("--autostash"))
+ (magit-reset nil)
+ (magit-stash nil))
